9b5265955eba67b095cdc4dfcccbe3193fec792c
Harold Seo <harold.seo.612@gmail.com>
2023-06-01 13:43:59 -0700

    Install LangChain

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { OpenAI } = require("langchain/llms/openai");
 
 app.get("/api", (req, res) => {
   res.json({ users: ["One", "Two", "Three", "Four"] });
