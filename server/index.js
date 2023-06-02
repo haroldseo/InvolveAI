d334a0435b2d77590ef4778df2c5a4790698b63f
Harold Seo <harold.seo.612@gmail.com>
2023-06-01 19:42:59 -0700

    Create Langchain Prompt

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 //Import necessary modules and libraries
 import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
 import express from "express";
 import * as dotenv from "dotenv";
 const app = express();
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 //Load environmental variables
 dotenv.config();
 
-//Create Model, allows us to connect to LLMs
+//Create Model, allows us to connect to LLMs, in this case, OpenAI API
 const runOpenAI = async () => {
   const model = new OpenAI({
     temperature: 0.9,
@@ -17,6 +18,16 @@ const runOpenAI = async () => {
   const openAIResponse = await model.call("What would be a good company name a company that makes colorful socks?");
   return openAIResponse;
 };
+
+//Generates dynamic prompt for model
+const generatePrompt = async () => {
+  const prompt = PromptTemplate.fromTemplate(
+    "If provided a short description of {this}, can you generate promotional content for it and tailor it for {social}?"
+  );
+  const promptResponse = await prompt.format({ this: "product", social: "Twitter" });
+  console.log(promptResponse);
+};
+generatePrompt();
 
 app.get("/api", async (req, res) => {
   const openAIResponse = await runOpenAI();
